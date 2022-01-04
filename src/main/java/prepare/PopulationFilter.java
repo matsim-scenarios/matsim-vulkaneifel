332ac9d94cc60e432502478a4595c88f09eca975
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-01-04 11:04:55 +0100

    PopulationFilter.java: created new class for filtering population, since the ShapePopulationFilter class could not handle MultiPolygons
    VulkaneifelApplication.java: added prepareConfig-method, note that activity types does not contain activity-times in double format, but in integer-format
    
    Makefile creates now the whole scenario without conflicts
    
    Furthermore several files, that belong to the dilutionArea shape has been added

---
+++ src/main/java/prepare/PopulationFilter.java
@@ -0,0 +1,128 @@
+package prepare;
+
+import com.conveyal.gtfs.model.Stop;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.options.CrsOptions;
+import org.matsim.application.options.ShpOptions;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.io.PopulationReader;
+import org.matsim.core.population.io.PopulationWriter;
+import org.matsim.core.router.TripStructureUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.ShapeFileReader;
+import picocli.CommandLine;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+@CommandLine.Command(
+        name="filter-population",
+        description = "Filter population by shape file",
+        showDefaultValues = true
+)
+
+public class PopulationFilter implements MATSimAppCommand {
+
+    private static final Logger log = LogManager.getLogger(ShapePopulationFilter.class);
+
+    @CommandLine.Parameters(paramLabel = "INPUT", arity = "1", description = "Path to population")
+    private Path input;
+
+    @CommandLine.Option(names = "--output", description = "name of output plans file", required = false)
+    private String output;
+
+    @CommandLine.Mixin
+    private ShpOptions shp = new ShpOptions();
+
+    @CommandLine.Mixin
+    private CrsOptions crs = new CrsOptions("EPSG:4326");
+
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new ShapePopulationFilter()).execute(args));
+    }
+    @Override
+    public Integer call() {
+
+        var populationPath = input;
+
+        if(output == null) output = input.toString();
+
+        var scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new PopulationReader(scenario).readFile(populationPath.toString());
+
+        Predicate<Activity> filter = (activity) -> true;
+        if (shp.getShapeFile() != null) {
+            // default input is set to lat lon
+            ShpOptions.Index index = shp.createIndex(crs.getInputCRS(), "_");
+            filter = (activity) -> !index.contains(activity.getCoord());
+        }
+
+        Predicate<Activity> finalFilter = filter;
+        var personsToRemove = scenario.getPopulation().getPersons().values().parallelStream()
+                .filter(person -> {
+                    var activities = TripStructureUtils.getActivities(person.getSelectedPlan(), TripStructureUtils.StageActivityHandling.ExcludeStageActivities);
+                       return activities.stream().anyMatch(finalFilter);
+
+                   // return activities.stream().anyMatch(activity -> !filterGeometry.covers(MGC.coord2Point(activity.getCoord())));
+                })
+                .collect(Collectors.toList());
+
+        log.info("Filter " + personsToRemove.size() + " of " + scenario.getPopulation().getPersons().size() + " persons.");
+        for (var person : personsToRemove) {
+            scenario.getPopulation().removePerson(person.getId());
+        }
+
+        new PopulationWriter(scenario.getPopulation()).write(output);
+
+        return 0;
+    }
+
+    private List<Geometry> getGeometries(String shapeFile){
+
+        return ShapeFileReader.getAllFeatures(shapeFile).stream().map(feature -> (Geometry)feature.getDefaultGeometry())
+                .collect(Collectors.toList());
+    }
+
+    private List<PreparedGeometry> getPreparedGeometries(String shapeFile){
+
+        return ShapeFileReader.getAllFeatures(shapeFile).stream().map(feature -> (Geometry)feature.getDefaultGeometry())
+                .map(geometry -> new PreparedGeometryFactory().create(geometry))
+                .collect(Collectors.toList());
+    }
+
+
+    private boolean isInGeometry(List<PreparedGeometry> geometries, Coord coord){
+        int counter = 0;
+
+        for (var geometry: geometries){
+
+            counter++;
+            if (geometry.covers(MGC.coord2Point(coord)))  {
+
+                log.info("Geometry #" + counter + " covered the activity");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private PreparedGeometry getPreparedGeometry(String shapeFile) {
+
+        return ShapeFileReader.getAllFeatures(shapeFile).stream()
+                .map(feature -> (Geometry)feature.getDefaultGeometry())
+                .map(geometry -> new PreparedGeometryFactory().create(geometry))
+                .findAny()
+                .orElseThrow();
+    }
+}
--- src/main/java/run/VulkaneifelApplication.java
+++ src/main/java/run/VulkaneifelApplication.java
@@ -1,13 +1,24 @@
 package run;
 
 import org.matsim.application.MATSimApplication;
+import org.matsim.application.prepare.population.CleanPopulation;
+import org.matsim.application.prepare.population.DownSamplePopulation;
+import org.matsim.application.prepare.population.ResolveGridCoordinates;
+import org.matsim.application.prepare.population.TrajectoryToPlans;
 import org.matsim.application.prepare.pt.CreateTransitScheduleFromGtfs;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
 import picocli.CommandLine;
 import prepare.CreateNetwork;
+import prepare.PopulationFilter;
+
+import java.util.List;
 
 @CommandLine.Command(header = ":: Open Vulkaneifel Scenario ::", version="1.0")
 @MATSimApplication.Prepare({
-        CreateNetwork.class, CreateTransitScheduleFromGtfs.class
+        CreateNetwork.class, CreateTransitScheduleFromGtfs.class, TrajectoryToPlans.class,
+        CleanPopulation.class, ResolveGridCoordinates.class, DownSamplePopulation.class, PopulationFilter.class
 })
 
 public class VulkaneifelApplication extends MATSimApplication {
@@ -16,4 +27,31 @@ public class VulkaneifelApplication extends MATSimApplication {
         MATSimApplication.run(VulkaneifelApplication.class, args);
     }
 
+    @Override
+    protected Config prepareConfig(Config config) {
+
+        for (long ii = 600; ii <= 97200; ii += 600) {
+
+            for (String act : List.of("business", "educ_higher", "educ_kiga", "educ_other", "educ_primary", "educ_secondary",
+                    "educ_tertiary", "errands", "home", "leasure", "shop_daily", "shop_other", "visit", "work")) {
+                config.planCalcScore()
+                        .addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams(act + "_" + ii + ".0").setTypicalDuration(ii));
+            }
+
+            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("work_" + ii + ".0").setTypicalDuration(ii)
+                    .setOpeningTime(6. * 3600.).setClosingTime(20. * 3600.));
+            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("business_" + ii + ".0").setTypicalDuration(ii)
+                    .setOpeningTime(6. * 3600.).setClosingTime(20. * 3600.));
+            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("leisure_" + ii + ".0").setTypicalDuration(ii)
+                    .setOpeningTime(9. * 3600.).setClosingTime(27. * 3600.));
+            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("shopping_" + ii + ".0").setTypicalDuration(ii)
+                    .setOpeningTime(8. * 3600.).setClosingTime(20. * 3600.));
+        }
+
+        config.plansCalcRoute().setAccessEgressType(PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLink);
+        config.qsim().setUsingTravelTimeCheckInTeleportation(true);
+        config.qsim().setUsePersonIdForMissingVehicleId(false);
+
+        return config;
+    }
 }
