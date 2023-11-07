6056dd88aa84dfb22538a01d92770c276d0caec8
rakow <rakow@vsp.tu-berlin.de>
2023-11-07 16:30:14 +0100

    update calibrate script

--- src/main/python/calibrate.py
+++ src/main/python/calibrate.py
@@ -31,10 +31,10 @@ if os.path.exists("mid.csv"):
 modes = ["walk", "car", "ride", "pt", "bike"]
 fixed_mode = "walk"
 initial = {
-    "bike": -3.0,
-    "pt": -3.0,
-    "car": -1.0,
-    "ride": -4.0
+    "bike": -2.16,
+    "pt": -0.5368,
+    "car": 0.0013103,
+    "ride": -2.619
 }
 
 # FIXME: Adjust
@@ -47,11 +47,8 @@ target = {
 }
 
 shp = gpd.read_file("../input/dilutionArea/dilutionArea.shp").set_crs("EPSG:25832")
-homes = pd.read_csv("../input/vulkaneifel-v1.1-homes.csv", dtype={"person": "str"})
-
 
 def filter_persons(persons):
-    persons = pd.merge(persons, homes, how="inner", left_on="person", right_on="person")
     persons = gpd.GeoDataFrame(persons, geometry=gpd.points_from_xy(persons.home_x, persons.home_y))
 
     df = gpd.sjoin(persons.set_crs("EPSG:25832"), shp, how="inner", op="intersects")
@@ -66,12 +63,12 @@ def filter_modes(df):
 
 # FIXME: Adjust paths and config
 
-study, obj = calibration.create_mode_share_study("calib", "../matsim-vulkaneifel-1.1-SNAPSHOT.jar",
-                                        "../input/vulkaneifel-v1.1-25pct.config.xml",
+study, obj = calibration.create_mode_share_study("calib", "../matsim-vulkaneifel-1.2-SNAPSHOT.jar",
+                                        "../input/v1.2/vulkaneifel-v1.2-25pct.config.xml",
                                         modes, target, 
                                         initial_asc=initial,
                                         args="--25pct --config:TimeAllocationMutator.mutationRange=900",
-                                        jvm_args="-Xmx60G -Xmx60G -XX:+AlwaysPreTouch",
+                                        jvm_args="-Xmx60G -Xmx60G -XX:+AlwaysPreTouch -XX:+UseParallelGC",
                                         lr=calibration.linear_lr_scheduler(start=0.5, interval=6),
                                         person_filter=filter_persons, map_trips=filter_modes, chain_runs=calibration.default_chain_scheduler)
 
